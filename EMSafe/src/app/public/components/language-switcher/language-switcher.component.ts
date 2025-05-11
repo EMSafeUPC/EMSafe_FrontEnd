407cef3ff50383f76d7882cbbd87ea6a31b05d83
Williams <164354079+WiJeGo@users.noreply.github.com>
2025-05-10 19:53:33 -0500

    feat: add language-switcher, styles, animation and responsive design

--- EMSafe/src/app/public/components/language-switcher/language-switcher.component.ts
+++ EMSafe/src/app/public/components/language-switcher/language-switcher.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import {MatButtonToggle, MatButtonToggleGroup} from '@angular/material/button-toggle';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-language-switcher',
-  imports: [],
+  imports: [
+    MatButtonToggleGroup,
+    MatButtonToggle,
+
+  ],
   templateUrl: './language-switcher.component.html',
   styleUrl: './language-switcher.component.css'
 })
 export class LanguageSwitcherComponent {
 
+  protected currentLang: string='en';
+  protected languages: string[]=['en','es'];
+
+
+  constructor(private translate: TranslateService) {
+    this.currentLang=this.translate.currentLang
+  }
+
+  useLanguages(languages:string):void {
+    this.translate.use(languages);
+    this.currentLang=languages;
+  }
 }
