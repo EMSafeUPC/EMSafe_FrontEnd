7bbd290be60499e35b15a0278f122933996457c3
Williams <164354079+WiJeGo@users.noreply.github.com>
2025-05-10 20:35:59 -0500

    feat(routes): configure application routes and server-side rendering setup

--- EMSafe/src/app/app.component.ts
+++ EMSafe/src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterOutlet, Routes} from '@angular/router';
+import {HeaderComponent} from './public/components/header/header.component';
+import {SidebarComponent} from './public/components/sidebar/sidebar.component';
+import {CommonModule} from '@angular/common';
+import {TranslateService} from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'EMSafe';
+
+  constructor(private translate: TranslateService) {
+    // Configurar idioma por defecto
+    translate.setDefaultLang('en');
+
+    // Usar el idioma del navegador si está disponible, de lo contrario usar el predeterminado
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang?.match(/en|es/) ? browserLang : 'en');
+  }
+
+
 }
--- EMSafe/src/app/app.routes.ts
+++ EMSafe/src/app/app.routes.ts
@@ -1,3 +1,49 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    },
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./public/pages/dashboard/dashboard.component').then(m => m.DashboardComponent)
+    },
+    {
+        path: 'devices/history',
+        loadComponent: () => import('./public/pages/devices/devices-history/devices-history.component').then(m => m.DevicesHistoryComponent)
+    },
+    {
+        path: 'devices/management',
+        loadComponent: () => import('./public/pages/devices/devices-management/devices-management.component').then(m => m.DevicesManagementComponent)
+    },
+    {
+        path: 'alarms/history',
+        loadComponent: () => import('./public/pages/alarms/alarms-history/alarms-history.component').then(m => m.AlarmsHistoryComponent)
+    },
+    {
+        path: 'alarms/settings',
+        loadComponent: () => import('./public/pages/alarms/alarms-settings/alarms-settings.component').then(m => m.AlarmsSettingsComponent)
+    },
+    {
+        path: 'map',
+        loadComponent: () => import('./public/pages/map/map.component').then(m => m.MapComponent)
+    },
+    {
+        path: 'profile',
+        loadComponent: () => import('./public/pages/profile/profile.component').then(m => m.ProfileComponent)
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./public/pages/login/login.component').then(m => m.LoginComponent)
+    },
+    {
+        path: 'settings',
+        loadComponent: () => import('./public/pages/settings/settings.component').then(m => m.SettingsComponent)
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard'
+    }
+];
