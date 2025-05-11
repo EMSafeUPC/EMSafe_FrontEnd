fbfe998b913045b7ae869674ed77c4a1cafa2dac
Williams <164354079+WiJeGo@users.noreply.github.com>
2025-05-10 23:31:42 -0500

    feat: add alarm card component UI

---
+++ EMSafe/src/app/public/shared/components/alarm-card/alarm-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alarm-card',
+  imports: [],
+  templateUrl: './alarm-card.component.html',
+  styleUrl: './alarm-card.component.css'
+})
+export class AlarmCardComponent {
+
+}
