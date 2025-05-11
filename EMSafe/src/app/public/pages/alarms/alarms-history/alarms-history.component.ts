5f6abec8bc4e4551e6abf805b144cd52f586741a
Williams <164354079+WiJeGo@users.noreply.github.com>
2025-05-10 20:24:09 -0500

    feat(header): integrate language switcher and improve sidebar toggle functionality

---
+++ EMSafe/src/app/public/pages/alarms/alarms-history/alarms-history.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alarms-history',
+  imports: [],
+  templateUrl: './alarms-history.component.html',
+  styleUrl: './alarms-history.component.css'
+})
+export class AlarmsHistoryComponent {
+
+}
---
+++ EMSafe/src/app/public/pages/alarms/alarms-settings/alarms-settings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alarms-settings',
+  imports: [],
+  templateUrl: './alarms-settings.component.html',
+  styleUrl: './alarms-settings.component.css'
+})
+export class AlarmsSettingsComponent {
+
+}
